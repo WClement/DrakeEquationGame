0a93c9adade51934b85123c1523ab630dd942bd6
Will Clement <William.w.clement@gmail.com>
2016-01-23 11:16:24 -0700

    added mvc

---
+++ src/model/Model.java
@@ -0,0 +1,19 @@
+package model;
+
+import controller.Controller;
+
+public class Model {
+
+	private Controller myController;
+	
+	public void setController(Controller myController) {
+		this.myController = myController;
+	}
+	
+	public Model() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	
+}
